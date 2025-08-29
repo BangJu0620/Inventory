b25bf8be9bb788e47e710e0b934a6eb331e93fdf
BangJu0620 <bju0620@naver.com>
2025-08-29 11:51:09 +0900

    [Chore] 불필요한 파일 제거

--- Assets/Scripts/Item/EquipItemData.cs
+++ Assets/Scripts/Item/EquipItemData.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equip", menuName = "Item/New Equip")]
 public class EquipItemData : ItemData   // 장비 가능 아이템
 {
-    // Stat이라는 클래스를 따로 만들어서 현재 수치, 최대 수치 감소 로직등을 만들면 좋을 것 같은데
-    // 그 스탯들을 모아서 컬렉션으로 만들면 관리하기 편할려나
-    // 딕셔너리로 만들어서 해당 스탯에 접근하기 편하게 만드는 게 좋나?
-
     [field: SerializeField] public float Attack { get; protected set; }
 
     [field: SerializeField] public float Deffense { get; protected set; }
